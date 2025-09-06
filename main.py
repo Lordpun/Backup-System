92ab2f3d23500023d519c9b651c309b0c0c7f206
Lucas Hartzell <hartzelllucas1@gmail.com>
2025-09-05 20:00:35 -0400

    Added a console menu

--- main.py
+++ main.py
@@ -24,3 +24,39 @@ def addBackupPath(path):
 	with open("backupPath.txt", "a") as paths:
 		paths.write(path)
 	paths.close()
+
+def consoleMenuStart():
+	print("\033[34mBACKUPS\n\033[0m")
+
+	print("Choose an option\n\033[90m1] Initalize backup system\n2] Add a path to backup\n3] Remove a path\n4] Manual backup\n\033[0m")
+
+	options = {
+		"1": "init",
+		"2": "add",
+		"3": "remove",
+		"4": "backup"
+	}
+
+	while True:
+		userInput = str(input()).lower()
+
+		try:
+			option = options[userInput]
+		except:
+			print("\033[31mPlease enter a listed number\033[0m")
+		else:
+			match option:
+				case "exit":
+					return "Exited Loop"
+				case "init":
+					return "Option: init"
+				case "add":
+					return "Option: add"
+				case "remove":
+					return "Option: remove"
+				case "backup":
+					return "Option: backup"
+				case _:
+					print("\033[31mError! We reached a case we shouldn't have reached.\033[0m")
+			
+inputVal = consoleMenuStart()
