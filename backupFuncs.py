3449cf2b445692d138b0664670a3b004cef15d73
Lucas Hartzell <hartzelllucas1@gmail.com>
2025-09-06 15:19:46 -0400

    Moved the backup releated functions to an external file, not connect to main.py yet

---
+++ backupFuncs.py
@@ -0,0 +1,55 @@
+def getBackupPaths():
+	pathList = []
+
+	with open("backupPath.txt") as paths:
+		for line in paths:
+			pathList.append(line.strip())
+		paths.close()
+
+	return pathList;
+
+def getBackupLocation():
+	locationPath = ""
+
+	with open("savePaths.txt", "r") as location:
+		locationPath = location.readLine().strip();
+	location.close()
+
+	if locationPath == "":
+		return "\033[31mError\033[0m: Empty path!, please run the initalize option first."
+
+	return locationPath
+
+def addBackupPath(path):
+	# Checking to see the file exists
+	paths = open("backupPath.txt", "x")
+	paths.close()
+
+	with open("backupPath.txt", "a") as paths:
+		paths.write(path)
+	paths.close()
+
+def removeBackupPath(path):
+	# There was probably a much simpler way to remove a line, but oh well.
+
+	# Turning text into an array
+	with open("backupPath.txt", "r") as paths:
+		pathsText = paths.readLines()
+	paths.close()
+
+	# Finding the path and removing it
+	for item in pathsList:
+		if item.strip() == path:
+			pathIndex = pathsText.index(item)
+			pathsText.pop(pathIndex)
+
+	# wiping the paths file
+	with open("backupPath.txt", "w") as paths:
+		pass
+	paths.close();
+
+	# Rewriting the content
+	with open("backupPath.txt", "a") as paths:
+		for item in pathsText:
+			paths.write(item)
+	paths.close()
