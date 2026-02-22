4107659dc9f5a680bdf0c8415a6a5a77bb5b269d
Lucas Hartzell <thedirtcart@gmail.com>
2026-02-22 00:07:47 -0500

    Added some styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import UtilityButton from "./components/utiltiyButton";
+import styles from './main.module.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
