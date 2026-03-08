bc3b45d542bd72268671704bb2ad2fc3d9b71a1d
Lordpun <thedirtcart@gmail.com>
2026-03-08 17:22:47 -0400

    Made functions to edit paths

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::sync::{LazyLock, Mutex};
+use std::path::PathBuf;
+
+pub static PATH_LIST: LazyLock<Mutex<Vec<PathBuf>>> = LazyLock::new(|| {
+	Mutex::new(Vec::new())
+});
+
+mod paths;
+use paths::{add_path, remove_path, send_paths};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![
+    	add_path,
+    	remove_path,
+    	send_paths
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
---
+++ src-tauri/src/paths.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+use crate::PATH_LIST;
+
+#[tauri::command]
+pub fn add_path(path_string: String) {
+	let mut path_list = PATH_LIST.lock().unwrap();
+	path_list.push(PathBuf::from(path_string));
+}
+
+#[tauri::command]
+pub fn remove_path(path_string: String) {
+	let mut path_list = PATH_LIST.lock().unwrap();
+	let target = PathBuf::from(path_string);
+	path_list.retain(|p| p != &target)
+}
+
+// For frontend to display paths
+#[tauri::command]
+pub fn send_paths() -> Vec<String> {
+	let path_list = PATH_LIST.lock().unwrap();
+
+	path_list
+	  .iter()
+	  .map(|path| path.to_string_lossy().into_owned())
+	  .collect()
+}
