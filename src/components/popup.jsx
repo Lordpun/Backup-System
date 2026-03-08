c4cbe45e6af2014167868bd3ebbc56eaea8bf04b
Lucas Hartzell <thedirtcart@gmail.com>
2026-03-07 23:27:23 -0500

    Added a popup component

---
+++ src/components/popup.jsx
@@ -0,0 +1,24 @@
+import styles from './popup.module.css';
+import UtilityButton from "./components/utiltiyButton";
+
+function Popup({ pageTitle, mainBody}) {
+	return(
+		<div className="background">
+
+		<div className="popup">
+			<header>
+				<h2>{ pageTitle }</h2>
+			</header>
+
+			<main>
+				<section>
+					{mainBody}
+				</section>
+
+				<UtilityButton text="Close" />
+			</main
+		</div>
+
+		</div>
+	)
+}
