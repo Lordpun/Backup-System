9617d186051e0aa538931799a3d3e044ec3a668b
Lordpun <thedirtcart@gmail.com>
2026-03-09 00:18:05 -0400

    Added a config syconfig system

--- src-tauri/src/backup.rs
+++ src-tauri/src/backup.rs
@@ -1,25 +1,29 @@
 use std::fs;
 use fs_extra::copy_items;
 use fs_extra::dir::CopyOptions;
+use crate::BACKUP_PATH;
 
 // The frontend will have code to detect whether something is a file or folder
 // Both backup a single path, rest done in the frontend to display progress easier
 
 #[tauri::command]
-pub fn backup_file(backup_path: String, file: String) -> Result<(), String> {
-	fs::copy(file, backup_path)
+pub fn backup_file(file: String) -> Result<(), String> {
+	let backup_path = BACKUP_PATH.lock().unwrap();
+	fs::copy(file, &*backup_path)
 	  .map(|_| ())
 	  .map_err(|e| e.to_string())
 }
 
 #[tauri::command]
-pub fn backup_folder(backup_path: String, folder: String) -> Result<(), String> {
+pub fn backup_folder(folder: String) -> Result<(), String> {
+	let backup_path = BACKUP_PATH.lock().unwrap();
+
 	let mut options = CopyOptions::new();
   options.overwrite = true;
   
 	let folder_items = vec![folder];
 
-  copy_items(&folder_items, backup_path, &options)
+  copy_items(&folder_items, &*backup_path, &options)
   	.map(|_| ())
   	.map_err(|e| e.to_string())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,10 +5,16 @@ pub static PATH_LIST: LazyLock<Mutex<Vec<PathBuf>>> = LazyLock::new(|| {
 	Mutex::new(Vec::new())
 });
 
+pub static BACKUP_PATH: LazyLock<Mutex<String>> = LazyLock::new(|| {
+	Mutex::new(String::new())
+});
+
 mod paths;
 mod backup;
+mod config;
 use paths::{add_path, remove_path, send_paths};
 use backup::{backup_file, backup_folder};
+use config::update_config;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -19,7 +25,8 @@ pub fn run() {
     	remove_path,
     	send_paths,
     	backup_file,
-    	backup_folder
+    	backup_folder,
+    	update_config
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
