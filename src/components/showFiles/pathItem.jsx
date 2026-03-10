c015521ceea68a7da9fa8990a8a6ea5e2282ba49
Lordpun <thedirtcart@gmail.com>
2026-03-09 20:01:03 -0400

    Connected remove_file to the frontend

--- src/components/showFiles/pathItem.jsx
+++ src/components/showFiles/pathItem.jsx
@@ -1,9 +1,14 @@
+import { invoke } from "@tauri-apps/api/core";
 import styles from "./pathItem.module.css";
 
 function pathItem({ filePath }) {
+	const removeFile = async () => {
+		await invoke("remove_file", { filePath });
+	}
+
 	return(
 	<div className={styles.path}>
-		<button>Remove</button>
+		<button onClick={removeFile}>Remove</button>
 
 		<p>{filePath}</p>
 	</div>
